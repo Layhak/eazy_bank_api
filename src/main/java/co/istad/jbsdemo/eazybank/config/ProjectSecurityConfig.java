cbde89e4326728f5b351fc4b48010b81a8dbbcd9
LAYHAK <layhak.garu@gmail.com>
2024-05-22 21:35:47 +0700

    init user using InMemoryUserDetailsManager with DefaultPasswordEncoder

--- src/main/java/co/istad/jbsdemo/eazybank/config/ProjectSecurityConfig.java
+++ src/main/java/co/istad/jbsdemo/eazybank/config/ProjectSecurityConfig.java
@@ -3,6 +3,9 @@ package co.istad.jbsdemo.eazybank.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -23,6 +26,21 @@ public class ProjectSecurityConfig {
                                         "/api/v1/bank/notice").permitAll())
                 .formLogin(withDefaults()).
                 httpBasic(withDefaults()).build();
+    }
+    //Only for testing
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin")
+                .authorities("ADMIN")
+                .build();
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("user")
+                .authorities("USER")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
     }
 
 }
